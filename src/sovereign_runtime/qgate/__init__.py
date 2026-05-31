ce72301893d2ba341b49461a710e01b33dda5fb7
MT Tech Industries LLC <mt.tech.industries@gmail.com>
2026-05-31 08:20:40 -0400

    Add Q Gate package initializer

---
+++ src/sovereign_runtime/qgate/__init__.py
@@ -0,0 +1,6 @@
+"""Q Gate continuity package."""
+
+from sovereign_runtime.qgate.q_gate import QGate, QGateResult
+from sovereign_runtime.qgate.continuity import ContinuityAssessment, ContinuityChecker
+
+__all__ = ["QGate", "QGateResult", "ContinuityAssessment", "ContinuityChecker"]
